044c15ae9314ee7e32923f7b9ac3138961dc79d7
Yassh2003 <yashpratapindia789@gmail.com>
2024-02-09 13:17:36 +0530

    Fixed Styling

--- src/Component/Header.jsx
+++ src/Component/Header.jsx
@@ -5,16 +5,16 @@ import { Link } from 'react-router-dom'
 function Header() {
   return (
     <HStack p= {"4"} shadow={"base"} bgColor={"black"} pos={"sticky"} top="0" left="0" zIndex={"1"}>
-      <Button variant={"unstyled"} color={"white"} marginRight={"8"}>
+      <Button variant={"unstyled"} color={"white"} marginRight={"7"}>
         <Link to="/">Home</Link>
       </Button>
-      <Button variant={"unstyled"} color={"white"} marginRight={"8"}>
+      <Button variant={"unstyled"} color={"white"} marginRight={"7"}>
         <Link to="/exchanges">Exchanges</Link>
       </Button>
-      <Button variant={"unstyled"} color={"white"} marginRight={"8"}>
+      <Button variant={"unstyled"} color={"white"} marginRight={"7"}>
         <Link to="/coins">Coins</Link>
       </Button>
-      <Button variant={"unstyled"} color={"white"}>
+      <Button variant={"unstyled"} color={"white"} marginRight={"10"}>
         <Link to="/blog">Blog</Link>
       </Button>
     </HStack>
